c0a301beae37036f42f291f1eb7ef7b78073b1d3
Josh Goldberg <josh@fullscreenmario.com>
2016-08-22 15:03:38 -0700

    Prototyped basic string formatting command logic

--- src/Commands/StringFormatCommand.ts
+++ src/Commands/StringFormatCommand.ts
@@ -1,4 +1,5 @@
 import { Command } from "./Command";
+import { CommandResult } from "./CommandResult";
 import { LineResults } from "./LineResults";
 import { Parameter } from "./Parameters/Parameter";
 import { RepeatingParameters } from "./Parameters/RepeatingParameters";
@@ -35,6 +36,58 @@ export class StringFormatCommand extends Command {
      * @returns Line(s) of code in the language.
      */
     public render(parameters: string[]): LineResults {
-        return LineResults.newSingleLine("", false);
+        if (this.language.properties.strings.formatting.useInterpolation) {
+            return this.renderInterpolation(parameters);
+        }
+
+        let output: string = parameters[0];
+        let inputsLength: number = this.getParameters.length / 2 - 1;
+
+        for (let i: number = 0; i < inputsLength; i += 1) {
+            let replacement: string = this.formatReplacement(i, parameters[i + 3]);
+
+            output = this.replaceFormatInput(output, i, replacement);
+        }
+
+        output = this.language.properties.strings.formatting.formatLeft + output;
+        output += this.language.properties.strings.formatting.formatRight;
+
+        return new LineResults([new CommandResult(output, 0)], true);
+    }
+
+    /**
+     * 
+     */
+    private formatReplacement(i: number, inputType: string): string {
+        let output: string = this.language.properties.strings.formatting.formatInputLeft;
+
+        if (this.language.properties.strings.formatting.formatInputNumbers) {
+            output += i;
+        }
+
+        if (this.language.properties.strings.formatting.inputTypes) {
+            if (this.language.properties.strings.formatting.typeCodes[inputType]) {
+                output += this.language.properties.strings.formatting.typeCodes[inputType];
+            } else {
+                output += inputType;
+            }
+        }
+
+        output += this.language.properties.strings.formatting.formatInputRight;
+        return output;
+    }
+
+    /**
+     * 
+     */
+    private replaceFormatInput(output: string, i: number, replacement: string): string {
+        return output.replace(new RegExp("\\{" + i + "\\}", "gi"), replacement);
+    }
+
+    /**
+     * 
+     */
+    private renderInterpolation(...args: any[]): LineResults {
+        return new LineResults([new CommandResult("", 0)], true);
     }
 }
