67dcc0d8574977ebc8bb20589ba5270b0a728109
Benjamin Wolf <bwolfgns@gmail.com>
2016-10-25 23:21:34 -0400

    Added Interface Declaration Implementation.
    Added all files related to interface declaration.

---
+++ src/Languages/Properties/InterfaceProperties.ts
@@ -0,0 +1,35 @@
+/**
+ * Metadata on a language's in depth interface properties.
+ */
+export class InterfaceProperties {
+
+    /**
+     * Keyword that indicates interface declaration.
+     */
+    public declareStartLeft: string;
+
+    /**
+     * Starts interface declaration.
+     */
+    public declareStartRight: string;
+
+    /**
+     * Keyword that indicates to extend from parent interfaces.
+     */
+    public declareExtendsLeft: string;
+
+    /**
+     * Seperator of multiple parent interfaces.
+     */
+    public declareExtendsRight: string;
+
+    /**
+     * Ends interface declaration.
+     */
+    public declareEnd: string;
+
+    /**
+     * Whether language supports interfaces.
+     */
+    public interfaceSupport: boolean;
+}
