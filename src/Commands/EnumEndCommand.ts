af2ea53840be849b8996a9d259a3b2ddcb8342e7
Benjamin Wolf <bwolfgns@gmail.com>
2016-12-13 02:24:58 -0500

    Fixed integration files.

--- src/Commands/EnumEndCommand.ts
+++ src/Commands/EnumEndCommand.ts
@@ -1,9 +1,26 @@
 import { BlockEndCommand } from "./BlockEndCommand";
+import { LineResults } from "./LineResults";
 
 /**
  * A command for ending an enum declaration.
  */
 export class EnumEndCommand extends BlockEndCommand {
+    /**
+     * Renders the command for a language with the given parameters.
+     * 
+     * @param parameters   The command's name, followed by any parameters.
+     * @returns Line(s) of code in the language.
+     */
+    public render(parameters: string[]): LineResults {
+        let ender: string = this.renderBlockEnd();
+
+        if (ender === "\0") {
+            return LineResults.newBlockLine("\0", -1);
+        }
+
+        return LineResults.newBlockLine(ender, -1);
+    }
+
     /**
      * Renders the end block for enums.
      * 
