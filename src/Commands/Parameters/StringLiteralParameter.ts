eede64a84dfc235183a771b97aa1bf29a53dc32f
chris-j-tang <chrisjtang98@gmail.com>
2017-06-10 13:10:45 -0400

    Added StringLiteralParameter subclass

---
+++ src/Commands/Parameters/StringLiteralParameter.ts
@@ -0,0 +1,23 @@
+import { Parameter } from "./Parameter";
+
+/**
+ * A string literal parameter.
+ */
+export class StringLiteralParameter extends Parameter {
+    /**
+     * The name of the string literal.
+     */
+    public name: string;
+
+    /**
+     * Initializes a new instance of the Parameter class.
+     * 
+     * @param descriptor   A plain-text description of the parameter.
+     * @param parameters   Parameters contained inside.
+     */
+    constructor(name: string, description: string) {
+        super(description);
+
+        this.name = name;
+    }
+}
