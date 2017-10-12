3ebc94840a2aa01f417cb7895f45c625a844b281
Josh Goldberg <joshuakgoldberg@outlook.com>
2017-10-12 12:40:08 -0700

    Converted .bind-based render switchers to classes (#390)

--- src/Commands/NativeCallCommand.ts
+++ src/Commands/NativeCallCommand.ts
@@ -1,8 +1,13 @@
 import { ConversionContext } from "../Conversions/ConversionContext";
 import { Import } from "../Languages/Imports/Import";
-import { NativeCallProperties, NativeCallScope, NativeCallType } from "../Languages/Properties/NativeCallProperties";
+import { NativeCallProperties, NativeCallScope } from "../Languages/Properties/NativeCallProperties";
 import { Command } from "./Command";
 import { LineResults } from "./LineResults";
+import { NativeArrayRenderer } from "./NativeCallRendering/NativeArrayRenderer";
+import { NativeCallRenderer } from "./NativeCallRendering/NativeCallRenderer";
+import { NativeMemberRenderer } from "./NativeCallRendering/NativeMemberRenderer";
+import { NativeOperatorRenderer } from "./NativeCallRendering/NativeOperatorRenderer";
+import { NativeStaticRenderer } from "./NativeCallRendering/NativeStaticRenderer";
 
 /**
  * A command for performing a native call, such as Array::push.
@@ -16,7 +21,7 @@ export abstract class NativeCallCommand extends Command {
     /**
      * Renderers for each allowed scope.
      */
-    private scopeRenderers: { [i: number]: (parameters: string[]) => LineResults };
+    private scopeRenderers: { [i: number]: NativeCallRenderer };
 
     /**
      * Initializes a new instance of the Command class.
@@ -27,11 +32,12 @@ export abstract class NativeCallCommand extends Command {
         super(context);
 
         this.nativeCallProperties = this.retrieveNativeCallProperties();
+
         this.scopeRenderers = {
-            [NativeCallScope.Array]: this.renderAsArray.bind(this),
-            [NativeCallScope.Member]: this.renderAsMember.bind(this),
-            [NativeCallScope.Operator]: this.renderAsOperator.bind(this),
-            [NativeCallScope.Static]: this.renderAsStatic.bind(this)
+            [NativeCallScope.Array]: new NativeArrayRenderer(this.nativeCallProperties),
+            [NativeCallScope.Member]: new NativeMemberRenderer(this.nativeCallProperties),
+            [NativeCallScope.Operator]: new NativeOperatorRenderer(this.nativeCallProperties),
+            [NativeCallScope.Static]: new NativeStaticRenderer(this.nativeCallProperties)
         };
     }
 
@@ -44,7 +50,7 @@ export abstract class NativeCallCommand extends Command {
      */
     public render(parameters: string[]): LineResults {
         const scope: NativeCallScope = this.nativeCallProperties.scope;
-        const results: LineResults = this.scopeRenderers[scope](parameters);
+        const results: LineResults = this.scopeRenderers[scope].render(parameters);
 
         results.addImports(this.retrieveImports());
 
@@ -62,135 +68,4 @@ export abstract class NativeCallCommand extends Command {
      * @returns Metadata on how to perform the native call.
      */
     protected abstract retrieveNativeCallProperties(): NativeCallProperties;
-
-    /**
-     * Formats a property argument for a native call, with "{0}" replaced
-     * with the first given parameter.
-     *
-     * @param argument   An argument for a native call.
-     * @param firstParameter   The first given parameter for the call.
-     * @returns The argument, with the parameter formatted inside.
-     */
-    private formatArgument(argument: string, firstParameter: string): string {
-        return argument.replace("{0}", firstParameter);
-    }
-
-    /**
-     * Renders the native call as an array.
-     *
-     * @param parameters   The command's name, followed by any number of
-     *                     items to initialize in the Array.
-     * @returns Line(s) of code in the language.
-     * @remarks Usage: [parameter, parameter].name
-     */
-    private renderAsArray(parameters: string[]): LineResults {
-        let result = "";
-
-        result += "[" + parameters[1];
-        for (let i = 2; i < parameters.length; i += 1) {
-            result += ", " + parameters[i];
-        }
-
-        result += "]." + this.nativeCallProperties.name;
-
-        return LineResults.newSingleLine(result, true);
-    }
-
-    /**
-     * Renders the native call as a member.
-     *
-     * @param parameters   The command's name, followed by any number of
-     *                     items to initialize in the Array.
-     * @returns Line(s) of code in the language.
-     * @remarks Usage: (name[, parameters, ...]).
-     */
-    private renderAsMember(parameters: string[]): LineResults {
-        let result = "";
-
-        result += parameters[1] + ".";
-        result += this.nativeCallProperties.name;
-
-        if (this.nativeCallProperties.type === NativeCallType.Function) {
-            result += "(";
-
-            if (this.nativeCallProperties.arguments.length > 0) {
-                result += this.formatArgument(this.nativeCallProperties.arguments[0], parameters[1]);
-
-                for (let i = 1; i < this.nativeCallProperties.arguments.length; i += 1) {
-                    result += ", " + this.formatArgument(this.nativeCallProperties.arguments[i], parameters[1]);
-                }
-            }
-
-            if (parameters.length > 2) {
-                if (this.nativeCallProperties.arguments.length > 0) {
-                    result += ", ";
-                }
-
-                result += parameters[2];
-
-                for (let i = 3; i < parameters.length; i += 1) {
-                    result += ", " + parameters[i];
-                }
-            }
-
-            result += ")";
-        }
-
-        return LineResults.newSingleLine(result, true);
-    }
-
-    /**
-     * Renders the native call as an operator.
-     *
-     * @param parameters   The command's name, followed by any number of
-     *                     items to initialize in the Array.
-     * @returns Line(s) of code in the language.
-     * @remarks Usage: (container, operand)
-     */
-    private renderAsOperator(parameters: string[]): LineResults {
-        let result = "";
-
-        if (this.nativeCallProperties.type === NativeCallType.FloatingLeft) {
-            result += parameters[2];
-            result += this.nativeCallProperties.name;
-            result += parameters[1];
-        } else {
-            result += parameters[1];
-            result += this.nativeCallProperties.name;
-            result += parameters[2];
-
-            if (parameters.length === 4) {
-                result += parameters[3];
-            }
-        }
-
-        return LineResults.newSingleLine(result, true);
-    }
-
-    /**
-     * Renders the native call as a static.
-     *
-     * @param parameters   The command's name, followed by any number of
-     *                     items to initialize in the Array.
-     * @returns Line(s) of code in the language.
-     * @remarks Usage: (name[, parameters, ...]).
-     */
-    private renderAsStatic(parameters: string[]): LineResults {
-        let result = "";
-
-        result += this.nativeCallProperties.name;
-        result += "(" + parameters[1];
-
-        for (const arg of this.nativeCallProperties.arguments) {
-            result += ", " + this.formatArgument(arg, parameters[1]);
-        }
-
-        for (let i = 2; i < parameters.length; i += 1) {
-            result += ", " + parameters[i];
-        }
-
-        result += ")";
-
-        return LineResults.newSingleLine(result, true);
-    }
 }
---
+++ src/Commands/NewRendering/MemberMethodRenderer.ts
@@ -0,0 +1,32 @@
+import { LineResults } from "../LineResults";
+import { NewRenderer } from "./NewRenderer";
+
+/**
+ * Renders a member method instantiation.
+ */
+export class MemberMethodRenderer extends NewRenderer {
+    /**
+     * Renders the instantation for a language with the given parameters.
+     *
+     * @param parameters   The command's name, followed by any number of parameters.
+     * @returns Line(s) of code in the language.
+     */
+    public render(parameters: string[]): LineResults {
+        let result = "";
+
+        const typeName: string = parameters[1];
+        result += typeName;
+        result += ".";
+        result += this.language.properties.newProp.keyword;
+        result += "(";
+        if (parameters.length > 2) {
+            result += parameters[2];
+            for (let i = 3; i < parameters.length; i += 1) {
+                result += ", " + parameters[i];
+            }
+        }
+        result += ")";
+
+        return LineResults.newSingleLine(result, false);
+    }
+}
