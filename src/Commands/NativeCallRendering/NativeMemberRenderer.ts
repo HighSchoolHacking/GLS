143cf1f633d76e6d52afca233fec36b5dc8723a5
Josh Goldberg <josh@fullscreenmario.com>
2017-10-12 12:37:10 -0700

    Converted .bind-based render switchers to classes

---
+++ src/Commands/NativeCallRendering/NativeMemberRenderer.ts
@@ -0,0 +1,49 @@
+import { NativeCallType } from "../../Languages/Properties/NativeCallProperties";
+import { LineResults } from "../LineResults";
+import { NativeCallRenderer } from "./NativeCallRenderer";
+
+/**
+ * Renders a native member call.
+ */
+export class NativeMemberRenderer extends NativeCallRenderer {
+    /**
+     * Renders the native call for a language with the given parameters.
+     *
+     * @param parameters   The command's name, followed by any parameters.
+     * @returns Line(s) of code in the language.
+     */
+    public render(parameters: string[]): LineResults {
+        let result = "";
+
+        result += parameters[1] + ".";
+        result += this.nativeCallProperties.name;
+
+        if (this.nativeCallProperties.type === NativeCallType.Function) {
+            result += "(";
+
+            if (this.nativeCallProperties.arguments.length > 0) {
+                result += this.formatArgument(this.nativeCallProperties.arguments[0], parameters[1]);
+
+                for (let i = 1; i < this.nativeCallProperties.arguments.length; i += 1) {
+                    result += ", " + this.formatArgument(this.nativeCallProperties.arguments[i], parameters[1]);
+                }
+            }
+
+            if (parameters.length > 2) {
+                if (this.nativeCallProperties.arguments.length > 0) {
+                    result += ", ";
+                }
+
+                result += parameters[2];
+
+                for (let i = 3; i < parameters.length; i += 1) {
+                    result += ", " + parameters[i];
+                }
+            }
+
+            result += ")";
+        }
+
+        return LineResults.newSingleLine(result, true);
+    }
+}
--- src/Commands/NewCommand.ts
+++ src/Commands/NewCommand.ts
@@ -6,6 +6,10 @@ import { LineResults } from "./LineResults";
 import { CommandMetadata } from "./Metadata/CommandMetadata";
 import { RepeatingParameters } from "./Metadata/Parameters/RepeatingParameters";
 import { SingleParameter } from "./Metadata/Parameters/SingleParameter";
+import { MemberMethodRenderer } from "./NewRendering/MemberMethodRenderer";
+import { MethodCallRenderer } from "./NewRendering/MethodCallRenderer";
+import { NewRenderer } from "./NewRendering/NewRenderer";
+import { PrefixRenderer } from "./NewRendering/PrefixRenderer";
 
 /**
  * Instantiates a new class instance.
@@ -28,7 +32,7 @@ export class NewCommand extends Command {
     /**
      * Renderers for each possible render style.
      */
-    private styleRenderers: { [i: number]: (parameters: string[]) => LineResults };
+    private styleRenderers: { [i: number]: NewRenderer };
 
     /**
      * Initializes a new instance of the Command class.
@@ -39,9 +43,9 @@ export class NewCommand extends Command {
         super(context);
 
         this.styleRenderers = {
-            [NewInstantiationSyntaxKind.MemberMethodCall]: this.renderMemberMethodCall.bind(this),
-            [NewInstantiationSyntaxKind.MethodCall]: this.renderMethodCall.bind(this),
-            [NewInstantiationSyntaxKind.Prefix]: this.renderPrefix.bind(this)
+            [NewInstantiationSyntaxKind.MemberMethodCall]: new MemberMethodRenderer(this.language),
+            [NewInstantiationSyntaxKind.MethodCall]: new MethodCallRenderer(this.language),
+            [NewInstantiationSyntaxKind.Prefix]: new PrefixRenderer(this.language)
         };
     }
 
@@ -60,82 +64,8 @@ export class NewCommand extends Command {
      * @returns Line(s) of code in the language.
      */
     public render(parameters: string[]): LineResults {
-        return this.styleRenderers[this.language.properties.newProp.instantiationKind](parameters);
-    }
+        const instantationKind = this.language.properties.newProp.instantiationKind;
 
-    /**
-     * Renders the command for a language in member method call style.
-     *
-     * @param parameters   The command's name, followed by any number of
-     *                     items to initialize in the Array.
-     * @returns Line(s) of code in the language.
-     */
-    private renderMemberMethodCall(parameters: string[]): LineResults {
-        let result = "";
-
-        const typeName: string = parameters[1];
-        result += typeName;
-        result += ".";
-        result += this.language.properties.newProp.keyword;
-        result += "(";
-        if (parameters.length > 2) {
-            result += parameters[2];
-            for (let i = 3; i < parameters.length; i += 1) {
-                result += ", " + parameters[i];
-            }
-        }
-        result += ")";
-
-        return LineResults.newSingleLine(result, false);
-    }
-
-    /**
-     * Renders the New command for a language in method call style.
-     *
-     * @param parameters   The command's name, followed by any number of
-     *                     items to initialize in the Array.
-     * @returns Line(s) of code in the language.
-     */
-    private renderMethodCall(parameters: string[]): LineResults {
-        const typeName: string = parameters[1];
-        let result = "";
-
-        result += typeName;
-        result += "(";
-        if (parameters.length > 2) {
-            result += parameters[2];
-            for (let i = 3; i < parameters.length; i += 1) {
-                result += ", " + parameters[i];
-            }
-        }
-        result += ")";
-
-        return LineResults.newSingleLine(result, false);
-    }
-
-    /**
-     * Renders the New command in prefix style.
-     *
-     * @param parameters   The command's name, followed by any number of
-     *                     items to initialize in the Array.
-     * @returns Line(s) of code in the language.
-     */
-    private renderPrefix(parameters: string[]): LineResults {
-        const typeName: string = parameters[1];
-        let result = "";
-
-        result += this.language.properties.newProp.keyword;
-        result += " ";
-        result += typeName;
-        result += "(";
-        if (parameters.length > 2) {
-            result += parameters[2];
-            for (let i = 3; i < parameters.length; i += 1) {
-                result += ", " + parameters[i];
-            }
-        }
-        result += ")";
-
-        return LineResults.newSingleLine(result, false);
+        return this.styleRenderers[instantationKind].render(parameters);
     }
 }
