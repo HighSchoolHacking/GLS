2fd1dc1877e55a2ac0511e72b706cd9c4d5faadb
Josh Goldberg <josh@fullscreenmario.com>
2018-10-28 00:08:42 -0400

    Logged java version

--- util/OutputGenerators/JavaOutputGenerator.ts
+++ util/OutputGenerators/JavaOutputGenerator.ts
@@ -7,5 +7,9 @@ import { spawnAndCaptureOutput } from "./Spawning";
  * Runs an output comparison test for a single GLS project in Java.
  */
 export const testJavaGenerator: IOutputGenerator = async ({ projectPath }): Promise<string[]> => {
+    // tslint:disable
+    const version = await spawnAndCaptureOutput("java", "--version");
+
+    console.log({ version });
     return spawnAndCaptureOutput("java", path.join(projectPath, "index.java"));
 };
