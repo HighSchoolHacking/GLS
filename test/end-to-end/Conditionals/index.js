b1eb93e9f04feb62aa26af637ce7f0ca26b11a60
Josh Goldberg <joshuakgoldberg@outlook.com>
2018-10-28 18:12:31 -0400

    Added output.txt to end-to-end Conditionals test (#509)
    Continues on #506.

---
+++ test/end-to-end/Conditionals/index.js
@@ -0,0 +1,20 @@
+//
+if (true) {
+    console.log("if true");
+}
+
+let second = false;
+if (second) {
+    console.log("wrong if variable");
+} else {
+    console.log("if false else");
+}
+
+if (1 + 1 === 3) {
+    console.log("wrong if operation");
+} else if (2 + 2 === 3) {
+    console.log("wrong else if");
+} else {
+    console.log("else if else");
+}
+//
