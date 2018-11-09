f0f854d5550c3aee1f1ea928e546eab18ea1e290
Josh Goldberg <josh@fullscreenmario.com>
2018-11-09 16:35:07 -0500

    Added auto-generation for .csproj files

--- util/OutputGenerators/CSharpOutputGenerator.ts
+++ util/OutputGenerators/CSharpOutputGenerator.ts
@@ -1,13 +1,27 @@
+import * as fs from "mz/fs";
 import * as path from "path";
 
 import { IOutputGenerator } from "./index";
 import { spawnAndCaptureOutput } from "./Spawning";
+
+const template = `<Project Sdk="Microsoft.NET.Sdk">
+  <PropertyGroup>
+    <NoWarn>0162,0414</NoWarn>
+    <OutputType>Exe</OutputType>
+    <TargetFramework>netcoreapp2.0</TargetFramework>
+  </PropertyGroup>
+</Project>
+`;
 
 /**
  * Runs an output comparison test for a single GLS project in C#.
  */
 export const testCSharpGenerator: IOutputGenerator = async ({ projectPath }): Promise<string[]> => {
     const basename = path.basename(projectPath);
+
+    await fs.writeFile(path.join(projectPath, basename + ".csproj"), template, {
+        flag: "w",
+    });
 
     return spawnAndCaptureOutput("dotnet", "run", "--project", path.join(projectPath, basename + ".csproj"));
 };
