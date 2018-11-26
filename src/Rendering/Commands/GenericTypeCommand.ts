ee8383ef249ed3a3d1bbd02e8e4f84bb73fd943b
Josh Goldberg <joshuakgoldberg@outlook.com>
2018-11-25 21:06:10 -0800

    Added missing type aliases and imports to generics (#627)
    Fixes #622.

--- src/Rendering/Commands/GenericTypeCommand.ts
+++ src/Rendering/Commands/GenericTypeCommand.ts
@@ -1,3 +1,4 @@
+import { Import } from "../Languages/Imports/Import";
 import { LineResults } from "../LineResults";
 import { CommandNames } from "../Names/CommandNames";
 import { Command } from "./Command";
@@ -33,12 +34,24 @@ export class GenericTypeCommand extends Command {
      * @returns Line(s) of code in the language.
      */
     public render(parameters: string[]): LineResults {
-        let output = parameters[1];
-
-        if (this.language.syntax.variables.explicitTypes) {
-            output += "<" + parameters.slice(2).join(", ") + ">";
+        if (!this.language.syntax.variables.explicitTypes) {
+            return this.context.convertParsed([CommandNames.Type, parameters[1]]);
         }
 
-        return LineResults.newSingleLine(output);
+        const imports: Import[] = [];
+        let output = parameters[1] + "<";
+
+        for (let i = 2; i < parameters.length; i += 1) {
+            const typeLine = this.context.convertParsed([CommandNames.Type, parameters[i]]);
+            output += typeLine.commandResults[0].text;
+            imports.push(...typeLine.addedImports);
+
+            if (i !== parameters.length - 1) {
+                output += ", ";
+            }
+        }
+
+        output += ">";
+        return LineResults.newSingleLine(output).withImports(imports);
     }
 }
